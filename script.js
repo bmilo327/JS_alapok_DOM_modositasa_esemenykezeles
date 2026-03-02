54f90bf5a55a70ab34e807e7d5b2131b59761622
bmilo327 <bernathmilan7924@gmail.com>
2026-03-02 21:31:28 +0100

    kész feladatok eseménykezelés 3-ig, képméret változtatás nem működik

--- script.js
+++ script.js
@@ -4,44 +4,78 @@ window.addEventListener("load", function () {
     elemekElerese3();
     elemekElerese4();
     elemekFormazasa1();
-    elemekFormazasa2();
-
+    esemenykezeles2();
+    
+    
+    
 });
 
+let h2Elem = document.querySelectorAll("section h2") [0];
+let ideIdElem = document.querySelectorAll("#ide") [0];
+let ideClassElem = document.querySelectorAll(".ide") [0];
+let divElem = document.querySelectorAll(".kattintasutan") [0];
+let feladatElem = document.querySelectorAll(".feladat") [0];
+const listaElem = document.querySelectorAll(".lista") [0];
+let kepElem = "<img id='kep' src='kepek/grass.jpeg'>";
+let kep = document.querySelector("#kep");
+
+esemenykezeles1();
+esemenykezeles3();
+
+
+
 function elemekElerese1() {
-   const h2Elem = document.querySelectorAll("section h2") [0];
-   console.log(h2Elem.innerHTML);
+    console.log(h2Elem.innerHTML);
 }
+
 function elemekElerese2() {
-   const ideElem = document.querySelectorAll("#ide") [0];
-   ideElem.innerHTML= "<p>Jó reggelt!</p>";
+    ideIdElem.innerHTML= "<p>Jó reggelt!</p>";
 }
+
 function elemekElerese3() {
-   const ideElem = document.querySelectorAll(".ide") [0];
-   ideElem.innerHTML= "<p>Jó reggelt!</p>";
+    ideClassElem.innerHTML= "<p>Jó reggelt!</p>";
 }
+
 function elemekElerese4() {
-   const listaElem = document.querySelectorAll(".lista") [0];
-   let tartalom = "<ul>";
-   for (let index = 0; index < 5; index++) {
+    let tartalom = "<ul>";
+    for (let index = 0; index < 5; index++) {
     let szamok = Math.floor(Math.random()*21+10);
     tartalom += `<li>${szamok}</li>`;
-   }
-   tartalom += "</ul>";
-   listaElem.innerHTML=tartalom;
+    }
+    tartalom += "</ul>";
+    listaElem.innerHTML = tartalom;
 }
+
 function elemekFormazasa1() {
-    const listaElem = document.querySelectorAll(".lista") [0];
     listaElem.classList.add("formazott")
 }
-function elemekFormazasa2() {
-    const listaElem = document.querySelectorAll(".lista") [0];
-    let divElem = document.getElementById(".kattintasutan")
-    divElem.addEventListener("click", fv);
-    function fv() {
-        divElem.innerHTML = listaElem.innerHTML; 
+
+function esemenykezeles1() {
+    listaElem.addEventListener("click", esemenykezeles1);
+    divElem.innerHTML = listaElem.innerHTML;    
+}
+
+function esemenykezeles2() {
+    feladatElem.innerHTML = "<button id='gomb'>Nyomj meg!</button>";
+    let gombElem = document.querySelector("#gomb");
+    gombElem.addEventListener("click", gomb);
+}
+
+function gomb() {
+    feladatElem.innerHTML += kepElem;
+}
+
+function esemenykezeles3() {
+    if (kep) {
+        feladatElem.addEventListener("mouseover", egerKepen);
     }
-    
-    console.log(divElem)
-    
+    feladatElem.addEventListener("mouseout", egerNincsKepen);
+}
+
+function egerKepen() {
+    kep.style.width = "50%";
+}
+
+function egerNincsKepen() {
+    kep.style.width = "100%";
 }
